957f146cbeca27da1018f491f8480ced66bb54a7
Bekhruzjon Hakmirzaev <hakmirzaevbekhruzjon@gmail.com>
2022-11-30 19:08:03 +0500

    designing UI

--- M5_L10_Task4/ContentView.swift
+++ M5_L10_Task4/ContentView.swift
@@ -6,83 +6,40 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        VStack(spacing: 10){
+            HStack{
+                Text("1").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.blue)
+                VStack(spacing: 10){
+                    Text("2").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.orange)
+                    Text("3").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.orange)
+                }.background(Color.black)
+            }
+            HStack{
+                Text("4").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.red)
+                Text("5").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.red)
+                Text("6").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.red)
+                Text("7").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.red)
+            }
+            HStack{
+                VStack{
+                    Text("8").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.blue)
+                    Text("9").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.blue)
                 }
-                .onDelete(perform: deleteItems)
+                Text("10").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.orange)
+                Text("11").foregroundColor(.white).frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.blue)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        }.background(Color.black)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
